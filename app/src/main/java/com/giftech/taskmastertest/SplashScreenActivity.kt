d6b27deb4ea375633813cbfc954e1969faef8d38
galihif <galihindra2401@mail.ugm.ac.id>
2021-05-01 13:59:46 +0700

    DISABLE DARK MODE

--- app/src/main/java/com/giftech/taskmastertest/SplashScreenActivity.kt
+++ app/src/main/java/com/giftech/taskmastertest/SplashScreenActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.giftech.taskmastertest.intro.Intro1Activity
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -11,6 +12,7 @@ class SplashScreenActivity : AppCompatActivity() {
     private var TIME_OUT:Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_splash_screen)
 
         loadSplashScreen()
